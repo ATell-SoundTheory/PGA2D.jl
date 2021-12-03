39f6e589425aecd3c7a5f2f084dc2f64e1406750
Documenter.jl <documenter@juliadocs.github.io>
2021-12-03 11:10:05 +0000

    build based on 54fde36

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PGA2D","category":"page"},{"location":"#PGA2D","page":"Home","title":"PGA2D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PGA2D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PGA2D]","category":"page"}]
+}
